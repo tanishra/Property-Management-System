c70b9af37f14c5650a29c9b79d909007752995d8
TANISH RAJPUT <100482827+tanishra@users.noreply.github.com>
2023-11-21 20:45:02 +0530

    Add files via upload

---
+++ Admin/MainPage.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/Mainpage.css">
+<title>Main Form </title>
+
+</head>
+<body style="background-image: url('back10.jpg');">
+<div class= "header sticky">
+    <h1 align="center" style="color: navy;">Property Management System</h1> 
+</div>
+<form action="" method="post">
+
+<table align="center" style="width:60%;">
+<tr><td <h5 align='center'><a href="view\Login.php"><p style="background-color:lightyellow;">Admin</p> </h5></td></tr>
+<tr><td <h5 align='center'><a href="http://localhost/Web_Tech/agent/View/AgentLogin.php"><p style="background-color:lightyellow;">Agent</p> </h5></td></tr>
+</table>
+
+
+
+</form>
+
+
+
+
+</body>
+</html>
